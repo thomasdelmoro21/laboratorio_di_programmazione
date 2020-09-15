ee41065c00cc48606a73d161cdce01e1e641a752
Thomas Del Moro <thomas.delmoro@stud.unifi.it>
2020-09-15 11:43:18 +0200

    Create le classi astratte Subject e Observer

---
+++ Observer.h
@@ -0,0 +1,18 @@
+//
+// Created by thomas on 15/09/20.
+//
+
+#ifndef LABORATORIO_DI_PROGRAMMAZIONE_OBSERVER_H
+#define LABORATORIO_DI_PROGRAMMAZIONE_OBSERVER_H
+
+
+class Observer {
+public:
+    virtual ~Observer() {}
+    virtual void attach() = 0;
+    virtual void detach() = 0;
+    virtual void update() = 0;
+};
+
+
+#endif //LABORATORIO_DI_PROGRAMMAZIONE_OBSERVER_H
---
+++ Subject.h
@@ -0,0 +1,19 @@
+//
+// Created by thomas on 15/09/20.
+//
+
+#ifndef LABORATORIO_DI_PROGRAMMAZIONE_SUBJECT_H
+#define LABORATORIO_DI_PROGRAMMAZIONE_SUBJECT_H
+
+#include "Observer.h"
+
+class Subject {
+public:
+    virtual ~Subject() {}
+    virtual void subscribe(Observer* o) = 0;
+    virtual void unsubscribe(Observer* o) = 0;
+    virtual void notify() = 0;
+};
+
+
+#endif //LABORATORIO_DI_PROGRAMMAZIONE_SUBJECT_H
