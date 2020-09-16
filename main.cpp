cd271dcc8e0b55b09183c9efd32bc957ac1f0fa9
Thomas Del Moro <thomas.delmoro@stud.unifi.it>
2020-09-16 19:00:39 +0200

    Istanziati dati sul main per verifica

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "User.h"
+#include "Chat.h"
+#include "Message.h"
+#include "MessageNotifier.h"
 
 int main() {
-
+    User antonio("Antonio");
+    User gianni("Gianni");
+    User paola("Paola");
+    Chat ag(antonio, gianni);
+    Chat ap(antonio, paola);
+    Chat* ptrAG = &ag;
+    Chat* ptrAP = &ap;
+    MessageNotifier notifierAG(ptrAG);
+    MessageNotifier notifierAP(ptrAP);
+    Message msg1(antonio, gianni, "ciao");
+    Message msg2(paola, antonio, "come stai");
+    ag.addMessage(msg1);  //nessuna notifica
+    ap.addMessage(msg2);  //notifica da Paola
 }
