0d80d8af861dbbebb67490125c732f52c5726041
Thomas Del Moro <thomas.delmoro@stud.unifi.it>
2020-09-16 19:08:50 +0200

    Creato il main di runAllTests

---
+++ test/runAllTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by thomas on 15/09/20.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
